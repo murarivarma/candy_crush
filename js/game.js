9b9be7a49fb82333cb6810888c8e73a66e4e9700
murarivarma <murarivarma@hotmail.com>
2016-01-29 10:46:15 +0530

    Populate cells with coordinates

--- js/game.js
+++ js/game.js
@@ -5,10 +5,36 @@ Game = {
   randomShapeClass: function() {
     return "fa-" + Game.shapes[Math.floor(Math.random() * Game.shapes.length)];
   },
-  init: function() {
+  populateCellsWithShapes: function() {
     return $.each($(".cell i"), function(i, element) {
       return $(element).addClass(Game.randomShapeClass);
     });
+  },
+  populateCellsWithCoordinates: function() {
+    var colNo, rowNo;
+    rowNo = 1;
+    colNo = 1;
+    $.each($("#board .row"), function(i, row) {
+      colNo = 1;
+      $.each($(row).children('.cell'), function(j, cell) {
+        cell.dataset.rowNo = rowNo;
+        cell.dataset.colNo = colNo;
+        return colNo++;
+      });
+      return rowNo++;
+    });
+    Game.rowsCount = rowNo - 1;
+    return Game.columnsCount = colNo - 1;
+  },
+  checkMatches: function() {
+    return console.log("Checking Matches");
+  },
+  init: function() {
+    Game.rowsCount = 0;
+    Game.columnsCount = 0;
+    Game.populateCellsWithShapes();
+    Game.populateCellsWithCoordinates();
+    return Game.checkMatches();
   }
 };
 
--- js/src/game.coffee
+++ js/src/game.coffee
@@ -4,8 +4,29 @@ Game =
   randomShapeClass: ->
     "fa-" + Game.shapes[Math.floor(Math.random()*Game.shapes.length)]
 
-  init: ->
+  populateCellsWithShapes: ->
     $.each $(".cell i"), (i, element) -> $(element).addClass Game.randomShapeClass
 
+  populateCellsWithCoordinates: ->
+      rowNo = 1
+      colNo = 1
+      $.each $("#board .row"), (i, row) ->
+        colNo = 1
+        $.each $(row).children('.cell'), (j, cell) ->
+          cell.dataset.rowNo = rowNo;
+          cell.dataset.colNo = colNo;
+          colNo++
+        rowNo++
+      Game.rowsCount = rowNo - 1
+      Game.columnsCount = colNo - 1
+  checkMatches: ->
+     console.log "Checking Matches"
+
+  init: ->
+    Game.rowsCount = 0
+    Game.columnsCount = 0
+    Game.populateCellsWithShapes()
+    Game.populateCellsWithCoordinates()
+    Game.checkMatches()
 $ ->
   Game.init()
